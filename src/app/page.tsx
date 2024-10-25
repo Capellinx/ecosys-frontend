4556e93bba0243a544899829a859523afd9b7f0b
capellinx <capellaaa7@gmail.com>
2024-10-25 20:08:20 -0300

    feat: redefine first page to use diferents layouts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,14 @@
 'use client'
 
-import { LoginForm } from "@/components/forms/login-form/login-form"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
-export default function AuthenticationPage() {
+export default function Page() {
+  const router = useRouter()
 
-  return (
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Bem-vindo de volta.
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Faça login com sua conta
-            </p>
-          </div>
-          <LoginForm />
-          {/* <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{" "}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Terms of Service
-            </Link>{" "}
-            and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
-              Privacy Policy
-            </Link>
-            .
-          </p> */}
-        </div>
-      </div>
-  )
+  useEffect(() => {
+    router.push("/login")
+  })
+
+  return null
 }
