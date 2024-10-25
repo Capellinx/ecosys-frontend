086236c4e72f5605f2d68a3943c09a20dadf1c96
capellinx <capellaaa7@gmail.com>
2024-10-25 13:41:28 -0300

    feat: create component to control register

---
+++ src/components/forms/register-form/register-form.tsx
@@ -0,0 +1,104 @@
+'use client'
+
+import { useState } from "react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Eye, EyeOff } from "lucide-react"
+import { UnityConservationList } from "@/components/unity-conservations-list"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+export function RegisterForm() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [showConfirmPassword, setshowConfirmPassword] = useState(false)
+
+  return (
+    <form className="space-y-4">
+      <div className="space-y-2">
+        <Label htmlFor="name">Nome Completo</Label>
+        <Input id="name" type="name" placeholder="exemplo@email.com" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" placeholder="exemplo@email.com" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="password">Senha</Label>
+        <div className="relative">
+          <Input
+            id="password"
+            type={showPassword ? "text" : "password"}
+            required
+          />
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            className="absolute right-2 top-1/2 -translate-y-1/2"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? (
+              <EyeOff className="h-4 w-4" />
+            ) : (
+              <Eye className="h-4 w-4" />
+            )}
+            <span className="sr-only">Toggle password visibility</span>
+          </Button>
+        </div>
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="confirmPassword">Confirmar Senha</Label>
+        <div className="relative">
+          <Input
+            id="confirmPassword"
+            type={showConfirmPassword ? "text" : "password"}
+            required
+          />
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            className="absolute right-2 top-1/2 -translate-y-1/2"
+            onClick={() => setshowConfirmPassword(!showConfirmPassword)}
+          >
+            {showConfirmPassword ? (
+              <EyeOff className="h-4 w-4" />
+            ) : (
+              <Eye className="h-4 w-4" />
+            )}
+            <span className="sr-only">Toggle password visibility</span>
+          </Button>
+        </div>
+      </div>
+      <UnityConservationList />
+      <div className="space-y-2">
+        <Label>Tipo de colaborador</Label>
+        <Select>
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Selecione um campo" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="Analista">Analista</SelectItem>
+            <SelectItem value="ATA - Agente temporário ambiental">ATA - Agente temporário ambiental</SelectItem>
+            <SelectItem value="Condutor">Condutor</SelectItem>
+            <SelectItem value="Pesquisador">Pesquisador</SelectItem>
+            <SelectItem value="Voluntario">Voluntario</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <Button type="submit" className="w-full font-bold bg-[#1E9E6A] hover:bg-[#1A714E] ease-in-out duration-300">
+        Cadastrar
+      </Button>
+    </form>
+  )
+}
+
+
+
