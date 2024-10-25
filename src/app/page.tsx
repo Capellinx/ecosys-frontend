a75c9f08eec6ff52f144c06562341b3e8de4a2c6
capellinx <capellaaa7@gmail.com>
2024-10-25 13:09:47 -0300

    feat: remove banner left and add in layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import { Eye, EyeOff } from "lucide-react"
@@ -14,25 +13,6 @@ export default function AuthenticationPage() {
   const [showPassword, setShowPassword] = useState(false)
 
   return (
-    <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0 p-10 sm:p-0">
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-black lg:flex dark:border-r">
-        <div className="absolute inset-0 overflow-hidden">
-          <Image
-            src="/svg/background-login.svg"
-            alt="Background Login"
-            className="object-cover"
-            fill
-            priority
-          />
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg text-[#1A714E]">
-              &ldquo;EcoSys, a ferramenta que muda a forma que você controla sua unidade de conservação.&rdquo;
-            </p>
-          </blockquote>
-        </div>
-      </div>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
@@ -118,6 +98,5 @@ export default function AuthenticationPage() {
           </p> */}
         </div>
       </div>
-    </div>
   )
 }
