d0f21adab00698743d6ccddb75eb11753e930877
capellinx <capellaaa7@gmail.com>
2024-10-30 01:10:08 -0300

    feaat: integrate hook form with inputs

--- src/components/forms/register-form/register-form.tsx
+++ src/components/forms/register-form/register-form.tsx
@@ -1,10 +1,10 @@
 'use client'
 
+import { z } from "zod"
 import { useState } from "react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import { Eye, EyeOff } from "lucide-react"
 import { UnityConservationList } from "@/components/unity-conservations-list"
 import {
@@ -14,89 +14,213 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { useForm } from "react-hook-form"
+import { registerFormSchema } from "./register-form.schema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { listCollaborators } from "./helpers/list-collaborators"
+import { maskCpf } from "@/utils/mask-cpf"
 
 export function RegisterForm() {
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPassword, setshowConfirmPassword] = useState(false)
 
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    defaultValues: {
+      name: "",
+      cpf: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      unity_conservation: ""
+    },
+    reValidateMode: "onChange",
+    resolver: zodResolver(registerFormSchema)
+  })
+
+  function submitRegister(data: z.infer<typeof registerFormSchema>) {
+    console.log(data);
+  }
+
   return (
-    <form className="space-y-4">
-      <div className="space-y-2">
-        <Label htmlFor="name">Nome Completo</Label>
-        <Input id="name" type="name" required />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="email">Email</Label>
-        <Input id="email" type="email" placeholder="exemplo@email.com" required />
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="password">Senha</Label>
-        <div className="relative">
-          <Input
-            id="password"
-            type={showPassword ? "text" : "password"}
-            required
-          />
-          <Button
-            type="button"
-            variant="ghost"
-            size="icon"
-            className="absolute right-2 top-1/2 -translate-y-1/2"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? (
-              <EyeOff className="h-4 w-4" />
-            ) : (
-              <Eye className="h-4 w-4" />
+    <>
+      <Form {...form}>
+        <form className="space-y-4" onSubmit={form.handleSubmit(submitRegister)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome Completo</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className={form.formState.errors.name ? "border-red-500" : ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
-            <span className="sr-only">Toggle password visibility</span>
-          </Button>
-        </div>
-      </div>
-      <div className="space-y-2">
-        <Label htmlFor="confirmPassword">Confirmar Senha</Label>
-        <div className="relative">
-          <Input
-            id="confirmPassword"
-            type={showConfirmPassword ? "text" : "password"}
-            required
           />
-          <Button
-            type="button"
-            variant="ghost"
-            size="icon"
-            className="absolute right-2 top-1/2 -translate-y-1/2"
-            onClick={() => setshowConfirmPassword(!showConfirmPassword)}
-          >
-            {showConfirmPassword ? (
-              <EyeOff className="h-4 w-4" />
-            ) : (
-              <Eye className="h-4 w-4" />
+
+          <FormField
+            control={form.control}
+            name="cpf"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>CPF</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={maskCpf(field.value)}
+                    placeholder="000.000.000-00"
+                    className={form.formState.errors.cpf ? "border-red-500" : ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
-            <span className="sr-only">Toggle password visibility</span>
+          />
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="exemplo@email.com"
+                    className={form.formState.errors.email ? "border-red-500" : ""}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl >
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      type={showPassword ? "text" : "password"}
+                      className={form.formState.errors.password ? "border-red-500" : ""}
+                    />
+                    <Button
+                      type="button"
+                      variant="link"
+                      size="icon"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 bg-none"
+                      onClick={() => setShowPassword(!showPassword)}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-4 w-4" />
+                      ) : (
+                        <Eye className="h-4 w-4" />
+                      )}
+                      <span className="sr-only">Toggle password visibility</span>
+                    </Button>
+                  </div>
+
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirmar Senha</FormLabel>
+                <FormControl >
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      type={showConfirmPassword ? "text" : "password"}
+                      className={form.formState.errors.confirmPassword ? "border-red-500" : ""}
+                    />
+                    <Button
+                      type="button"
+                      variant="link"
+                      size="icon"
+                      className="absolute right-2 top-1/2 -translate-y-1/2 bg-none"
+                      onClick={() => setshowConfirmPassword(!showConfirmPassword)}
+                    >
+                      {showConfirmPassword ? (
+                        <EyeOff className="h-4 w-4" />
+                      ) : (
+                        <Eye className="h-4 w-4" />
+                      )}
+                      <span className="sr-only">Toggle password visibility</span>
+                    </Button>
+                  </div>
+
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <UnityConservationList
+            control={form.control}
+          />
+
+          <FormField
+            control={form.control}
+            name="collaborator"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tipo de colaborador</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className={form.formState.errors.collaborator ? "border-red-500 w-full" : "w-full"}>
+                      <SelectValue placeholder="Selecione um campo" />
+                    </SelectTrigger>
+                    <SelectContent ref={field.ref}>
+                      {listCollaborators.map((collaborator, _index) => (
+                        <SelectItem
+                          value={collaborator}
+                          key={_index}
+                        >
+                          {collaborator}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="w-full font-bold bg-[#1E9E6A] hover:bg-[#1A714E] ease-in-out duration-300"
+          >
+            Cadastrar
           </Button>
-        </div>
-      </div>
-      <UnityConservationList />
-      <div className="space-y-2">
-        <Label>Tipo de colaborador</Label>
-        <Select>
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Selecione um campo" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="Analista">Analista</SelectItem>
-            <SelectItem value="ATA - Agente temporário ambiental">ATA - Agente temporário ambiental</SelectItem>
-            <SelectItem value="Condutor">Condutor</SelectItem>
-            <SelectItem value="Pesquisador">Pesquisador</SelectItem>
-            <SelectItem value="Voluntario">Voluntario</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <Button type="submit" className="w-full font-bold bg-[#1E9E6A] hover:bg-[#1A714E] ease-in-out duration-300">
-        Cadastrar
-      </Button>
-    </form>
+        </form>
+      </Form>
+    </>
   )
 }
 
