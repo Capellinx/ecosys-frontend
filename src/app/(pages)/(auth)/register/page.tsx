6ceecf1f40f4eb952523df9db888f41652de2fcc
capellinx <capellaaa7@gmail.com>
2024-10-30 03:13:45 -0300

    fea: define padding-top to card

--- src/app/(pages)/(auth)/register/page.tsx
+++ src/app/(pages)/(auth)/register/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
 
   return (
       <div className="lg:p-8 h-screen overflow-auto">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="mx-auto pt-20 flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Bem-vindo ao EcoSys.
