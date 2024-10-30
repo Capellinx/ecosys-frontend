6af78d91d82eeaafea43620551d7dfc8ed4ae20a
capellinx <capellaaa7@gmail.com>
2024-10-30 03:10:38 -0300

    feat: create hook to register collaborator

---
+++ src/components/forms/register-form/hooks/use-register-collaborator.tsx
@@ -0,0 +1,33 @@
+
+import { useMutation } from "@tanstack/react-query"
+import { z } from "zod"
+import { registerFormSchema } from "../register-form.schema"
+import { api } from "@/services/api"
+import { AxiosError } from "axios"
+
+export function useRegisterCollaborator() {
+
+   const { mutate, isPending } = useMutation({
+      mutationKey: ["registerCollaborator"],
+      mutationFn: async (values: z.infer<typeof registerFormSchema>) => {
+         await onSubmitCollaborator(values)
+      }
+   })
+
+   async function onSubmitCollaborator(values: z.infer<typeof registerFormSchema>): Promise<void> {
+      try {
+         const { data } = await api.post("/collaboratosr", values)
+
+         return data
+      } catch (error) {
+         if(error instanceof AxiosError) {
+            console.log("Deu ruim")
+         }
+      }
+
+   }
+   return {
+      handleRegisterCollaborator: mutate,
+      isLoading: isPending
+   }
+}
