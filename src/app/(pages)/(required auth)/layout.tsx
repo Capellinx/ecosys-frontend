d8328ba76fa09dfc760edc2a7557fad2983b407d
capellinx <capellaaa7@gmail.com>
2024-11-10 22:13:06 -0300

    feat: update import

--- src/app/(pages)/(required auth)/layout.tsx
+++ src/app/(pages)/(required auth)/layout.tsx
@@ -5,7 +5,7 @@ import {
    SidebarInset,
    SidebarProvider,
 } from "@/components/ui/sidebar"
-import { AppSidebar } from './dashboard/components/app-sidebar';
+import { AppSidebar } from './dashboard/fragments/app-sidebar';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
    return(
